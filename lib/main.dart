8b3a6ca654152f54cfcc5b2ca6bda247c08bfcec
Songhui Zhang <275185288@qq.com>
2018-12-18 23:39:59 +0800

    横向列表  拆分组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main ()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'viewList',
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('ViewList'),
+          appBar: new AppBar(
+            title: new Text('ViewList'),
+          ),
+          body: Container(height: 200, child: MyList())),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        Container(
+          width: 100,
+          color: Colors.green,
         ),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-              leading: new Icon(Icons.access_alarm),
-              title: new Text('access_alarm'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.access_time),
-              title: new Text('access_time'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.adb),
-              title: new Text('adb'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.book),
-              title: new Text('book'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.brightness_3),
-              title: new Container(
-                child: new Image.network('https://avatars2.githubusercontent.com/u/21076725?s=460&v=4'),
-                width: 40.0,
-                height: 35.0,
-                alignment: Alignment.topLeft,
-              ),
-            )
-          ],
+        Container(
+          width: 100,
+          color: Colors.red,
         ),
-      ),
+        Container(
+          width: 100,
+          color: Colors.pink,
+        ),
+        Container(
+          width: 100,
+          color: Colors.grey,
+        ),
+        Container(
+          width: 100,
+          color: Colors.lightBlue,
+        ),
+        Container(
+          width: 100,
+          color: Colors.cyan,
+        ),
+      ],
     );
   }
 }
