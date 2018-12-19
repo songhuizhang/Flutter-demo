1d8ec88bc306d8c52627a7f64be5627a79266f41
Songhui Zhang <275185288@qq.com>
2018-12-19 23:01:00 +0800

    渲染数据为纵向列表

--- lib/main.dart
+++ lib/main.dart
@@ -16,39 +16,53 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context,int index) {
-    return Text(posts[index].name);
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].url,),
+          SizedBox(
+            height: 16.0,
+          ),
+          Text(posts[index].name, style: Theme.of(context).textTheme.title),
+          Text(posts[index].rate, style: Theme.of(context).textTheme.subhead),
+          SizedBox(
+            height: 16.0,
+          )
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('SONGHUI'),
-          elevation: 0.0,
-        ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ),
-      );
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('SONGHUI'),
+        elevation: 0.0,
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _listItemBuilder,
+      ),
+    );
   }
 }
 
 class Hello extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-
-      return Center(
+  Widget build(BuildContext context) {
+    return Center(
       child: Text(
         'Hello',
         textDirection: TextDirection.rtl,
         style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.black87
-        ),
+            fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.black87),
       ),
-    );;
-    }
+    );
+    ;
+  }
 }
