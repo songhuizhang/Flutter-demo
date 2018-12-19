4fcbba9694e5d7a7a4bb0da0be88959c4524bb26
Songhui Zhang <275185288@qq.com>
2018-12-20 00:33:22 +0800

    添加抽屉子内容

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Home(),
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
-        splashColor: Colors.white70
-      ),
+          primarySwatch: Colors.yellow,
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+          splashColor: Colors.white70),
     );
   }
 }
@@ -26,58 +25,92 @@ class Home extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: '菜单',
-            onPressed: () => debugPrint('点击了菜单按钮'),
-          ),
-          title: Text('橙汁菌'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: '搜索',
-              onPressed: () => debugPrint('点击了搜索按钮'),
-            )
-          ],
-          elevation: 0.0,
-          bottom: TabBar(
-            unselectedLabelColor: Colors.black26,
-            indicatorColor: Colors.black54,
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorWeight: 1.0,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.email),
-              ),
-              Tab(
-                icon: Icon(Icons.phone),
-              ),
-              Tab(
-                icon: Icon(Icons.blur_circular),
-              ),
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: '菜单',
+              onPressed: () => debugPrint('点击了菜单按钮'),
+            ),
+            title: Text('橙汁菌'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: '搜索',
+                onPressed: () => debugPrint('点击了搜索按钮'),
+              )
             ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black26,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.email),
+                ),
+                Tab(
+                  icon: Icon(Icons.phone),
+                ),
+                Tab(
+                  icon: Icon(Icons.blur_circular),
+                ),
+              ],
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Icon(Icons.email, size: 130.0, color: Colors.black12),
-            Icon(Icons.phone, size: 130.0, color: Colors.black12),
-            Icon(Icons.blur_circular, size: 130.0, color: Colors.black12),
-          ],
-        ),
-        drawer: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          body: TabBarView(
             children: <Widget>[
-              Text('这是抽屉')
+              Icon(Icons.email, size: 130.0, color: Colors.black12),
+              Icon(Icons.phone, size: 130.0, color: Colors.black12),
+              Icon(Icons.blur_circular, size: 130.0, color: Colors.black12),
             ],
           ),
-        ),
-      ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.pink),
+                ),
+                ListTile(
+                  title: Text(
+                    '主页',
+                    textAlign: TextAlign.left,
+                  ),
+                  leading: Icon(Icons.home),
+                ),
+                ListTile(
+                  title: Text(
+                    '归档',
+                    textAlign: TextAlign.left,
+                  ),
+                  leading: Icon(Icons.archive),
+                ),
+                ListTile(
+                  title: Text(
+                    '分类',
+                    textAlign: TextAlign.left,
+                  ),
+                  leading: Icon(Icons.category),
+                ),
+                ListTile(
+                  title: Text(
+                    '标签',
+                    textAlign: TextAlign.left,
+                  ),
+                  leading: Icon(Icons.filter_b_and_w),
+                ),
+                 ListTile(
+                  title: Text(
+                    '设置',
+                    textAlign: TextAlign.left,
+                  ),
+                  leading: Icon(Icons.settings),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
