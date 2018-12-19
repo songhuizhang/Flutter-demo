ebbd736eeded743bdee2abaf628b74e822fb1e3b
Songhui Zhang <275185288@qq.com>
2018-12-19 22:45:48 +0800

    模拟豆瓣电影数据，创建列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
+import 'model/data.dart';
 
-void main() =>
-    runApp(MyApp(items: List<String>.generate(100, (i) => "this is $i")));
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-  MyApp({Key key, @required this.items}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'viewList',
-      home: new Scaffold(
-          body: new ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return new ListTile(
-                  title: Text('${items[index]}'),
-                );
-              })),
+      home: Home(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context,int index) {
+    return Text(posts[index].name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('SONGHUI'),
+          elevation: 0.0,
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        ),
+      );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+
+      return Center(
+      child: Text(
+        'Hello',
+        textDirection: TextDirection.rtl,
+        style: TextStyle(
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87
+        ),
+      ),
+    );;
+    }
 }
---
+++ lib/model/data.dart
@@ -0,0 +1,64 @@
+class Post {
+  const Post({
+    this.name,
+    this.url,
+    this.rate
+  });
+
+  final String name ;
+  final String url ;
+  final String rate ;
+}
+
+final List<Post> posts= [
+  Post(
+    name: "海王",
+    url: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541280047.webp",
+    rate: "7.9"
+  ),
+  Post(
+    name: "片警宝音",
+    url: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2541310016.webp",
+    rate: "0"
+  ),
+  Post(
+    name: "龙猫",
+    url: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2540924496.webp",
+    rate: "9.1"
+  ),
+  Post(
+    name: "中国合伙人2",
+    url: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541742368.webp",
+    rate: "4.2"
+  ),
+  Post(
+    name: "印度合伙人",
+    url: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2540940050.webp",
+    rate: "7.7"
+  ),
+  Post(
+    name: "网络谜踪",
+    url: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2542848758.webp",
+    rate: "8.5"
+  ),
+  Post(
+    name: "绿毛怪格林奇",
+    url: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2539666559.webp",
+    rate: "6.4"
+  ),
+   Post(
+    name: "无名之辈",
+    url: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2539661066.webp",
+    rate: "8.2"
+  ),
+   Post(
+    name: "狗十三",
+    url: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2540513831.webp",
+    rate: "8.4"
+  ),
+   Post(
+    name: "毒液：致命守护者",
+    url: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2537158013.webp",
+    rate: "7.3"
+  ),
+];
