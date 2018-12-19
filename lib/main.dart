fdd08c95fee91e6e4f356ef49d63763a1c42c7fc
Songhui Zhang <275185288@qq.com>
2018-12-20 00:14:13 +0800

    添加功能按键，并修改样式

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'model/data.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,23 +11,63 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70
+      ),
     );
   }
 }
 
 class Home extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('SONGHUI'),
-        elevation: 0.0,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: '菜单',
+            onPressed: () => debugPrint('点击了菜单按钮'),
+          ),
+          title: Text('橙汁菌'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: '搜索',
+              onPressed: () => debugPrint('点击了搜索按钮'),
+            )
+          ],
+          elevation: 0.0,
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black26,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.email),
+              ),
+              Tab(
+                icon: Icon(Icons.phone),
+              ),
+              Tab(
+                icon: Icon(Icons.blur_circular),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.email, size: 130.0, color: Colors.black12),
+            Icon(Icons.phone, size: 130.0, color: Colors.black12),
+            Icon(Icons.blur_circular, size: 130.0, color: Colors.black12),
+          ],
+        ),
       ),
-      body: null,
     );
   }
 }
-
