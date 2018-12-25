9d28b829c74024f29903b292faae87596c273063
Songhui Zhang <275185288@qq.com>
2018-12-25 20:15:38 +0800

    修改底部导航为可变状态类型

--- lib/demo/bottonnav_demo.dart
+++ lib/demo/bottonnav_demo.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 
-class BottomNavDemo extends StatelessWidget {
+class BottomNavDemo extends StatefulWidget {
+  _BottomNavDemo createState() => _BottomNavDemo();
+}
+
+class _BottomNavDemo extends State<BottomNavDemo> {
+  int _currentIndex = 0;
+  void _onTapHandler (int index) {
+    setState((){
+      _currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          fixedColor: Colors.green,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.edit),
-              title: Text('edit'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('my'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              title: Text('List'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.phone),
-              title: Text('phone'),
-            ),
-          ],
-        );
+      currentIndex: _currentIndex,
+      onTap: _onTapHandler,
+      type: BottomNavigationBarType.fixed,
+      fixedColor: Colors.black,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.edit),
+          title: Text('edit'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          title: Text('my'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          title: Text('List'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.phone),
+          title: Text('phone'),
+        ),
+      ],
+    );
   }
 }
+
